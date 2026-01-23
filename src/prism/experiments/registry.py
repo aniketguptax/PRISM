cc764a3b84ae88e6944e5ee080e183f1dfba8812
aniket1101 <1.aniketgupta@gmail.com>
2026-01-23 14:34:04 +0000

    feat: found computational closure and degradation case

--- src/prism/experiments/registry.py
+++ src/prism/experiments/registry.py
@@ -1,9 +1,10 @@
-from prism.processes import GoldenMean, EvenProcess, IIDBernoulli, MarkovOrder1, SimpleNonUnifilarSource
+from prism.processes import GoldenMean, EvenProcess, IIDBernoulli, MarkovOrder1, MarkovOrder2, SimpleNonUnifilarSource
 from prism.reconstruction import OneStepGreedyMerge
 
 PROCESS_REGISTRY = {
     "iid_bernoulli": IIDBernoulli,
-    "markov_order1": MarkovOrder1,
+    "markov_order_1": MarkovOrder1,
+    "markov_order_2": MarkovOrder2,
     "golden_mean": GoldenMean,
     "even_process": EvenProcess,
     "sns": SimpleNonUnifilarSource,
